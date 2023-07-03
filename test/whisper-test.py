ea7745fb402f6829442d111479417a01ada524e1
trollonion03 <trollonion03@gmail.com>
2023-07-04 01:10:37 +0900

    [Edit] khe - Add audio test example

---
+++ test/whisper-test.py
@@ -0,0 +1,17 @@
+import whisper as ws
+
+model = ws.load_model("base")
+
+#이걸 구해야 할건디,,,,
+audio = ws.load_audio("audio.mp3")
+audio = ws.pad_or_trim(audio)
+
+mel = ws.log_mel_spectrogram(audio).to(model.device)
+
+_, probs = model.detect_language(mel)
+print(f'Detected language: {max(probs, key=probs.get)}')
+
+options = ws.DecodingOptions()
+result = ws.decode(model, mel, options)
+
+print(result.text)
