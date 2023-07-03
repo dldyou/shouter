1bdd33f273e00c74d4d01df5fe0395086f9acf81
trollonion03 <trollonion03@gmail.com>
2023-07-04 01:29:24 +0900

    [Add] trollonion03 - Add test sample

--- test/whisper-test.py
+++ test/whisper-test.py
@@ -3,7 +3,7 @@ import whisper as ws
 model = ws.load_model("base")
 
 #이걸 구해야 할건디,,,,
-audio = ws.load_audio("audio.mp3")
+audio = ws.load_audio("test.m4a")
 audio = ws.pad_or_trim(audio)
 
 mel = ws.log_mel_spectrogram(audio).to(model.device)
