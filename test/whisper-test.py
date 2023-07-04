2c515a478dc432341bbf2344c66874aeb80f86fb
trollonion03 <trollonion03@gmail.com>
2023-07-04 23:29:28 +0900

    [Edit] trollonion03 - Edit test example

--- test/whisper-test.py
+++ test/whisper-test.py
@@ -1,9 +1,12 @@
 import whisper as ws
+import os
+
+print(os.getcwd())
 
 model = ws.load_model("base")
 
 #이걸 구해야 할건디,,,,
-audio = ws.load_audio("test.m4a")
+audio = ws.load_audio('C:\\Code\\shouter\\shouter\\shouter\\test\\test2.m4a')
 audio = ws.pad_or_trim(audio)
 
 mel = ws.log_mel_spectrogram(audio).to(model.device)
@@ -11,7 +14,7 @@ mel = ws.log_mel_spectrogram(audio).to(model.device)
 _, probs = model.detect_language(mel)
 print(f'Detected language: {max(probs, key=probs.get)}')
 
-options = ws.DecodingOptions()
+options = ws.DecodingOptions(fp16 = False)
 result = ws.decode(model, mel, options)
 
 print(result.text)
