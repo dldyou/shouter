7c63265b47ec5f4ee0f49bda6fd6f638e833fdb7
dldyou <127181255+dldyou2@users.noreply.github.com>
2023-07-08 00:32:22 +0900

    [Add] dldyou - trim MP3 file

---
+++ test/trim.py
@@ -0,0 +1,19 @@
+import os
+from pydub import AudioSegment
+import math
+
+filename = 'audio.mp3'
+standard_path = os.path.dirname(os.path.realpath(__file__)) + '\\'
+
+audio = AudioSegment.from_mp3(standard_path + filename)
+
+sec = 1000
+# trim cycle
+timing = 20 * sec
+# interpolation time
+interpolation_timing = 5 * sec
+
+# trim like 0-25 20-45 40-65
+for i in range(int(math.floor(len(audio) / timing))):
+    part_audio = audio[timing * i:timing * (i + 1) + interpolation_timing]
+    part_audio.export(standard_path + f'audio{i}.mp3')
