cf8666347580f2758e1dfb21362bb03bf5567fbe
dldyou <127181255+dldyou2@users.noreply.github.com>
2023-07-08 01:05:49 +0900

    [Edit] dldyou - style changed

--- test/whisper-test.py
+++ test/whisper-test.py
@@ -2,27 +2,37 @@ import whisper as ws
 import os
 
 # print(os.getcwd())
-filename = 'test2.m4a'
-path = os.path.dirname(os.path.realpath(__file__)) + '\\' + filename
+def start(filename):
+    path = os.path.dirname(os.path.realpath(__file__)) + '\\' + filename
+    
+    if (not os.path.isfile(path)):
+        return
+    
+    model = ws.load_model("medium")
 
-model = ws.load_model("base")
+    #이걸 구해야 할건디,,,,
+    audio = ws.load_audio(path)
+    audio = ws.pad_or_trim(audio)
 
-#이걸 구해야 할건디,,,,
-audio = ws.load_audio(path)
-audio = ws.pad_or_trim(audio)
+    mel = ws.log_mel_spectrogram(audio).to(model.device)
 
-mel = ws.log_mel_spectrogram(audio).to(model.device)
+    _, probs = model.detect_language(mel)
+    print(f'Detected language: {max(probs, key=probs.get)}')
 
-_, probs = model.detect_language(mel)
-print(f'Detected language: {max(probs, key=probs.get)}')
+    options = ws.DecodingOptions(fp16 = False)
+    # result = ws.decode(model, mel, options)
+    result = model.transcribe(audio)
 
-options = ws.DecodingOptions(fp16 = False)
-# result = ws.decode(model, mel, options)
-result = model.transcribe(audio)
+    #print(type(result))
+    # print(result['segments'])
 
-#print(type(result))
-# print(result['segments'])
-
-for data in result['segments']:
-    print(data)
-
+    for data in result['segments']:
+        start_time = data['start']
+        end_time = data['end']
+        text = data['text']
+        print(f'{start_time} ~ {end_time}: {text}')
+        
+idx = 0
+while (True):
+    start(f'audio{idx}.mp3')
+    idx += 1
