c0f7fbb0ad5097abba6c4c8679dc081d4b0057e2
trollonion03 <trollonion03@gmail.com>
2023-07-17 22:52:10 +0900

    [Edit] trollonion03 - Add HDFS file uplaod handler

--- test/upload.py
+++ test/upload.py
@@ -10,3 +10,12 @@ def file_processing():
     trim.trim()
     
     # upload to hdfs
+    spark = SparkSession.builder.getOrCreate()
+
+    audio_data = spark.sparkContext.binaryFiles('./audio0.mp3').first()[1]
+    dataframe = spark.createDataFrame(["hdfs://localhost:19000/data/audio0.mp3", audio_data], ["path", "data"])
+    
+    dataframe.write.mode("overwrite").format("binaryFile").option("path", "hdfs://localhost:19000/data/audio0.mp3").save()
+
+    
+
