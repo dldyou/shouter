22a85dc9ab1dd7cc7884d27b2aa81ab6a6fc7214
trollonion03 <trollonion03@gmail.com>
2023-07-19 22:14:12 +0900

    [Edit] trollonion03 - fix file upload handler

--- test/upload.py
+++ test/upload.py
@@ -16,18 +16,29 @@ def file_processing():
     trim.trim()
     
     #defines
-    filename = 'audio0.mp3'
-    filepath = os.path.dirname(os.path.realpath(__file__)) + '/' + filename
     hdfs_port = '9870'
     hdfs_url = f'http://localhost:{hdfs_port}'    
 
     # upload to hdfs
     # spark = SparkSession.builder.getOrCreate()
     hdfs_client = InsecureClient(hdfs_url)
-    hdfs_client.upload("/data/", filepath, overwrite=True)
 
-    #check upload status
-    upload_stat = hdfs_client.status(f'/data/{filename}')
-    print(upload_stat)
+    cnt = 0
+    while (True): 
+        filename = f'audio{cnt}.mp3'
+        path = os.path.dirname(os.path.realpath(__file__)) + '\\' + filename
+        
+        # file does not exist -> break loop
+        if (not os.path.isfile(path)):
+            break
+        
+        filepath = os.path.dirname(os.path.realpath(__file__)) + '/' + filename
+        hdfs_client.upload("/data/", filepath, overwrite=True)
+        
+        #check upload status
+        upload_stat = hdfs_client.status(f'/data/{filename}')
+        print(upload_stat)
+        
+        cnt += 1
 
 file_processing()
