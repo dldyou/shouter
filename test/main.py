ba98ab7b566f9f77d3fb696311ab28f007b63648
dldyou <60469410+dldyou@users.noreply.github.com>
2023-07-19 22:36:43 +0900

    [Add] dldyou - seperate file (main py)

---
+++ test/main.py
@@ -0,0 +1,18 @@
+import upload
+
+def main():
+    # file upload to hdfs like audio0.mp3, audio1.mp3, ..., audio{n}.mp3
+    upload.file_processing()
+    # file read from hdfs 
+
+    # set RDD 
+    
+    # Map (apply subtitle.get_subtitle() for every audio{n}.mp3 files)
+    
+    # Reduce (collect results)
+    
+    # transform to subtitle file form
+    
+    # start download subtitle file
+if __name__ == '__main__':
+    main()
