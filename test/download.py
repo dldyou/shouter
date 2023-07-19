3be8e75ced4dbc4462e1b9f79ea9a5ebdde80322
trollonion03 <trollonion03@gmail.com>
2023-07-19 23:50:29 +0900

    [Add] trollonion03 - Add hdfs file downloader

---
+++ test/download.py
@@ -0,0 +1,21 @@
+import os
+from hdfs import InsecureClient
+from hdfs import HdfsError
+
+def download(filename):
+    hdfs_port = '9870'
+    hdfs_url = f'http://localhost:{hdfs_port}'    
+    filepath = os.path.dirname(os.path.realpath(__file__))
+
+    # upload to hdfs
+    hdfs_client = InsecureClient(hdfs_url)
+    try:
+        hdfs_client.download(f'/data/{filename}', filepath)
+        
+        #check download status
+        if (os.path.isfile(filepath+'\\'+filename)):
+            print(f'File download Succesfully ({filename})')
+    except HdfsError as e:
+        print(str(e))
+
+download('audio3.mp3')
