788ad9c3a0e02c392d3ed50c299c7881aca5b7f8
sj030 <hsj3151120@konkuk.ac.kr>
2023-07-19 23:36:29 +0900

    [Edit] FoFbb - add get_path()

--- test/hdfsPath.py
+++ test/hdfsPath.py
@@ -8,14 +8,8 @@ from pyspark.sql.functions import udf
 from pyspark.sql.types import BinaryType
 from pydub import AudioSegment
 
-# Define a UDF to read audio file
-@udf(returnType=BinaryType())
-def read_audio_file(path):
-    audio = AudioSegment.from_file(path)
-    return audio.export(format="mp3").read()
-
 def get_path():
     spark = SparkSession.builder.getOrCreate()
-
-    df = spark.read.format("binaryFile").load("/data/audio0.mp3")
-    # .selectExpr("path", "read_audio_file(content) as audio_data")
+    df = spark.read.format("binaryFile").load("hdfs://localhost:50070/data/*.mp3").selectExpr("path").collect()
+    list = [row['path'] for row in df]
+    return list
