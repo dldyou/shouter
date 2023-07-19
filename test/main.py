d68bd5bb5a43f61909f3aa5154949ef2837dc51f
dldyou <60469410+dldyou@users.noreply.github.com>
2023-07-19 23:13:12 +0900

    [Edit] dldyou - set rdd / Map / Reduce

--- test/main.py
+++ test/main.py
@@ -1,19 +1,25 @@
+from pyspark import SparkConf, SparkContext
 import upload
+import task
 import hdfsPath as hp
 
 def main():
+    # configuration spark API
+    conf = SparkConf().setAppName("AudioProcessing").setMaster("yarn")
+    sctx = SparkContext(conf = conf)
     # file upload to hdfs like audio0.mp3, audio1.mp3, ..., audio{n}.mp3
     upload.file_processing()
     # file read from hdfs 
     audio_files = hp.get_path()
     # set RDD 
-    
+    audio_files_rdd = sctx.parallelize(audio_files)
     # Map (apply subtitle.get_subtitle() for every audio{n}.mp3 files)
-    
+    result_rdd = audio_files_rdd.map(task.process_audio_file)
     # Reduce (collect results)
-    
+    results = result_rdd.collect()
     # transform to subtitle file form
     
     # start download subtitle file
+    
 if __name__ == '__main__':
     main()
