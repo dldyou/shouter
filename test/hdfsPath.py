ed84fa6b7fe174f78710201deb5fd0d55c36beb7
dldyou <60469410+dldyou@users.noreply.github.com>
2023-07-19 23:47:55 +0900

    [Edit] change variable name list -> path

--- test/hdfsPath.py
+++ test/hdfsPath.py
@@ -4,12 +4,8 @@ findspark.init()
 import pyspark
 from pyspark.sql import SparkSession
 
-from pyspark.sql.functions import udf
-from pyspark.sql.types import BinaryType
-from pydub import AudioSegment
-
 def get_path():
     spark = SparkSession.builder.getOrCreate()
     df = spark.read.format("binaryFile").load("hdfs://localhost:50070/data/*.mp3").selectExpr("path").collect()
-    list = [row['path'] for row in df]
-    return list
+    path = [row['path'] for row in df]
+    return path
