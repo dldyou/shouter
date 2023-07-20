1f22e231beb2c72d88ba0c85550ed3f17e25dff7
dldyou <60469410+dldyou@users.noreply.github.com>
2023-07-20 23:17:58 +0900

    [Edit] dldyou - fix index error

--- test/merge.py
+++ test/merge.py
@@ -6,13 +6,13 @@ def merge_audio(result, cnt):
         start_time = result[i][-1][0]
         if (trim.timing - start_time <= 0.2):
             for sentence in result[i][:-1]:
-                sentence[0] += trim.timing * cnt
-                sentence[1] += trim.timing * cnt
+                sentence[0] += trim.timing * i
+                sentence[1] += trim.timing * i
                 ret.append(sentence)
         else:
             for sentence in result[i]:
-                sentence[0] += trim.timing * cnt
-                sentence[1] += trim.timing * cnt
+                sentence[0] += trim.timing * i
+                sentence[1] += trim.timing * i
                 ret.append(sentence)
     
     return ret
