2b852283cfb09a561198f2601fee93d71f500217
dldyou <60469410+dldyou@users.noreply.github.com>
2023-07-20 22:52:19 +0900

    [Edit] dldyou - fix time error

--- test/merge.py
+++ test/merge.py
@@ -6,9 +6,13 @@ def merge_audio(result, cnt):
         start_time = result[i][-1][0]
         if (trim.timing - start_time <= 0.2):
             for sentence in result[i][:-1]:
+                sentence[0] += trim.timing * cnt
+                sentence[1] += trim.timing * cnt
                 ret.append(sentence)
         else:
             for sentence in result[i]:
+                sentence[0] += trim.timing * cnt
+                sentence[1] += trim.timing * cnt
                 ret.append(sentence)
     
     return ret
