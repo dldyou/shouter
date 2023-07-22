e5ad3cba9e7f6e05d072557d8a29d730d13ec809
dldyou <60469410+dldyou@users.noreply.github.com>
2023-07-23 00:41:06 +0900

    [Edit] dldyou - code style (tab size 2 -> 4)

--- platform/server/static/script.js
+++ platform/server/static/script.js
@@ -2,32 +2,30 @@ const fileloadBtn = $("#fileload-btn")[0];
 const serverloadBtn = $("#upload-btn")[0]; // button for server
 const fileName = $("#file-name");
 
-function changeFileBox()
-{
-  fileName.text(fileloadBtn.files[0].name);
+function changeFileBox() {
+    fileName.text(fileloadBtn.files[0].name);
 }
 
-function uploadFile(){
+function uploadFile() {
+    var form = fileloadBtn.files[0];
+    var formData = new FormData(form[0]);
 
-  var form = fileloadBtn.files[0];
-  var formData = new FormData(form[0]);
-
-  const req = $.ajax({
-    type:"POST",
-    url: "/upload",
-    processData: false,
-    contentType: false,
-    data: formData,
-    success: function(rtn){
-      console.log("rtn: ", rtn)
-    },
-    err: function(err){
-      console.log("err:", err)
-    }
-  })
+    const req = $.ajax({
+        type: "POST",
+        url: "/upload",
+        processData: false,
+        contentType: false,
+        data: formData,
+        success: function(rtn) {
+            console.log("rtn: ", rtn)
+        },
+        err: function(err) {
+            console.log("err:", err)
+        }
+    })
 }
 
 $(document).ready(() => {
-  fileloadBtn.addEventListener("change", changeFileBox);
-  serverloadBtn.addEventListener("click", uploadFile);
+    fileloadBtn.addEventListener("change", changeFileBox);
+    serverloadBtn.addEventListener("click", uploadFile);
 });
